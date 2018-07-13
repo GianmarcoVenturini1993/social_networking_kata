085b2ba4e372046eeb746bc3ffa7ee747b9392b6
GianmarcoVenturini1993 <gianmarco.venturini@studenti.unimi.it>
2018-07-13 12:15:29 +0200

    Added java class for test and set dependencies in build gradle - good to go

---
+++ src/test/java/com/kata/socialnetworking/Test.java
@@ -0,0 +1,17 @@
+package com.kata.socialnetworking;
+
+
+import org.junit.Ignore;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+public class Test {
+
+    @org.junit.Test
+    @Ignore
+    public void test() {
+        fail("Not yet implemented");
+    }
+}
