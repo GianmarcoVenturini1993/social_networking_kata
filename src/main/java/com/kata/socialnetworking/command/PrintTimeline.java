193565450f033f8887b9056ce34961d570a694f8
GianmarcoVenturini1993 <gianmarco.venturini@outlook.it>
2018-07-18 17:51:49 +0200

    refactoring

--- src/main/java/com/kata/socialnetworking/command/PrintTimeline.java
+++ src/main/java/com/kata/socialnetworking/command/PrintTimeline.java
@@ -4,6 +4,16 @@ import com.kata.socialnetworking.UserFactory;
 
 import java.text.ParseException;
 
+/*
+Maybe I should insert here the createUser method in an if branch:
+    if(UserFactory.getUser(cmd) == null)
+        create user...
+    else
+        do print timeline
+
+...but for now, it's not required...design for change
+
+ */
 public class PrintTimeline implements CommandInterface {
     @Override
     public void doCommand(String cmd, String delimiter) {
